f86264aad287540f020604763f9f77c485113bc0
Kirin Pörsti <117549816+KirinPoersti@users.noreply.github.com>
2025-11-30 16:36:01 +0200

    Refactor TennisGame: add constants, improve variable names, extract methods

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,68 @@
 class TennisGame:
+    LOVE = 0
+    FIFTEEN = 1
+    THIRTY = 2
+    FORTY = 3
+    WIN_THRESHOLD = 4
+    
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.player1_score += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_score += 1
+
+    def _score_to_name(self, score):
+        """Convert numeric score to tennis score name."""
+        if score == self.LOVE:
+            return "Love"
+        elif score == self.FIFTEEN:
+            return "Fifteen"
+        elif score == self.THIRTY:
+            return "Thirty"
+        elif score == self.FORTY:
+            return "Forty"
+        return ""
+
+    def _get_tied_score(self):
+        """Get score text when both players have equal points."""
+        if self.player1_score == self.LOVE:
+            return "Love-All"
+        elif self.player1_score == self.FIFTEEN:
+            return "Fifteen-All"
+        elif self.player1_score == self.THIRTY:
+            return "Thirty-All"
+        else:
+            return "Deuce"
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        if self.player1_score == self.player2_score:
+            return self._get_tied_score()
+        elif self.player1_score >= self.WIN_THRESHOLD or self.player2_score >= self.WIN_THRESHOLD:
+            return self._get_endgame_score()
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            return self._get_regular_score()
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+    def _get_endgame_score(self):
+        """Get score when game is at advantage or win stage."""
+        score_difference = self.player1_score - self.player2_score
+        
+        if score_difference == 1:
+            return "Advantage player1"
+        elif score_difference == -1:
+            return "Advantage player2"
+        elif score_difference >= 2:
+            return "Win for player1"
+        else:
+            return "Win for player2"
 
-        return score
+    def _get_regular_score(self):
+        """Get score when game is in regular stage (before advantage)."""
+        player1_name = self._score_to_name(self.player1_score)
+        player2_name = self._score_to_name(self.player2_score)
+        return f"{player1_name}-{player2_name}"
